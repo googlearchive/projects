9b51b1f90a605f61269b3a8721276ffd3c4576d8
Steve Orvell <steve@orvellia.com>
2013-07-09 16:52:47 -0700

    using binding introspection provided by mdv.

--- Sandbox/lib/binding.js
+++ Sandbox/lib/binding.js
@@ -5,69 +5,15 @@
  */
 
 (function() {
-  // override Polymer property binding
-  var api = Polymer.api.instance.properties;
-  var bindProperty = api.bindProperty;
-  api.bindProperty = function(property, model, path) {
-    bindProperty.call(this, property, model, path);
-    registerBinding(this, property, path);
-  };
-  
-  var unbindProperty = api.unbindProperty;
-  api.unbindProperty = function(type, name) {
-    var result = unbindProperty(type, name);
-    if (result) {
-      unregisterBinding(this, name);
-    }
-    return result;
-  };
-  
-  // bind tracking
-  var bindings = new SideTable();
-
-  function registerBinding(element, name, path) {
-    var b$ = bindings.get(element);
-    if (!b$) {
-      bindings.set(element, b$ = {});
-    }
-    b$[name.toLowerCase()] = path;
-  }
-
-  function unregisterBinding(element, name) {
-    var b$ = bindings.get(element);
-    if (b$) {
-      delete b$[name.toLowerCase()];
+ function getBinding(element, name) {
+    if (element && element.bindings) {
+      var binding = element.bindings[name];
+      if (binding) {
+        return binding.path;
+      }
     }
   }
-
-  function overrideBinding(ctor) {
-    var proto = ctor.prototype;
-    var originalBind = proto.bind;
-    var originalUnbind = proto.unbind;
-
-    proto.bind = function(name, model, path) {
-      originalBind.apply(this, arguments);
-      // note: must do this last because mdv may unbind before binding
-      registerBinding(this, name, path);
-    };
-
-    proto.unbind = function(name) {
-      originalUnbind.apply(this, arguments);
-      unregisterBinding(this, name);
-    };
-  };
-
-  [Node, Element, Text, HTMLInputElement].forEach(overrideBinding);
-  
-  var emptyBindings = {};
-
-  function getBindings(element) {
-    return element && bindings.get(element) || emptyBindings;
-  }
-
-  function getBinding(element, name) {
-    return getBindings(element)[name.toLowerCase()];
-  }
+ 
   
   window.Bindings = {
     getBinding: getBinding
